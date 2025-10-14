9368ff228115808c654cace5e6fa4380a56042d4
AwesomePermissions <awesomepermissions@gmail.com>
2025-10-14 21:55:13 +0200

    Append trailing slash to `fetchClientSecret` redirect URL for route consistency.

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:3000/api';
+export const API_URL = 'https://dev-api.brickify.fun/api';
 export const PROJECT_NAME = 'Brickify';
 export const GOOGLE_CLIENT_ID = '176288432372-fvs96cuencuauhojab08lapu3t8c0iip.apps.googleusercontent.com';
 export const STRIPE_PUBLISHABLE_KEY = 'pk_test_51RxuwCFIUx31jlb7NwFwILU4JG944HXB6IE8tObyRjILzzgRpdydR8rz4XrmluhDvcXuGMLBJ29OL4gvKEkSafOo000cSlN3Hi';
