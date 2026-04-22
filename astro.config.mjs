8546c4b40418c3e9ccc60b8b1949ae85a62c0f2c
Claudy <contact@brickify.fun>
2026-04-22 23:21:27 +0200

    Automate sitemap generation with Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,17 @@
 
 import tailwindcss from '@tailwindcss/vite';
 import {defineConfig} from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://brickify.fun',
     output: 'static',
     vite: {
         plugins: [tailwindcss()],
     },
 
-    integrations: [react()],
+    integrations: [react(), sitemap()],
 });
